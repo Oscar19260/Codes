06c3a375ab7a4dc10b8cd40f929ee2dc2d2aa97a
Oscar19260 <61431500+Oscar19260@users.noreply.github.com>
2021-03-21 18:37:10 -0600

    Delete JavaScript directory

--- JavaScript/hero/story_start.js
+++
@@ -1,8 +0,0 @@
-//Setup website here
-let hero = document.querySelector("#hero");
-let villain = document.querySelector("#villain");
-let lightning = document.querySelector("#lightning");
-
-//Setup animation code here
-
-//Run animation code here
